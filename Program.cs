b3808e1876dab58b5d98e8089402a44e8693f5cb
Piotr Ćwikliński <piotr.cwiklinski88@gmail.com>
2022-05-23 20:57:30 +0200

    Ukończone wszystkie metody w klasie "Wydział" + kilka w innych klasach;

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Prog_obiektowe_WSB_Projekt
 {
@@ -9,62 +10,147 @@ namespace Prog_obiektowe_WSB_Projekt
 
     class Wydzial
     {
+        private List<Jednostka> jednostki = new List<Jednostka>();
+        private List<Przedmiot> przedmioty = new List<Przedmiot>();
+        private List<Student> studenci = new List<Student>();
+
         public void DodajJednostke(string nazwa, string adres)
         {
+            jednostki.Add(new Jednostka(nazwa, adres));
+            Console.WriteLine("Dodano nową jednostkę o nazwie \"" + nazwa + "\";");
 
         }
 
         public void DodajPrzedmiot(Przedmiot p)
         {
-
+            przedmioty.Add(p);
+            Console.WriteLine("Dodano nowy przedmiot o nazwie \"" + p.Nazwa  + "\";");
         }
 
         public void DodajStudenta(Student s)
         {
-
+            studenci.Add(s);
+            Console.WriteLine("Dodano nowego studenta, który nazywa się \"" + s.ImieNazwisko + "\";");
         }
 
         public bool DodajWykladowce(Wykladowca w, string nazwaJednostki)
         {
-            return false;
+            bool czyIstniejeTakaJednostka = false;
+            foreach (Jednostka jednostka in jednostki)
+            {
+                if (jednostka.Nazwa == nazwaJednostki)
+                {
+                    czyIstniejeTakaJednostka = true;
+                    jednostka.DodajWykladowce(w);
+                }
+            }
+            return czyIstniejeTakaJednostka;
         }
 
         public void InfoStudenci(bool infoOceny)
         {
-
+            int lp = 1;
+            foreach(Student s in studenci)
+            {
+                Console.WriteLine(lp + ") ");
+                s.WypiszInfo();
+                if(infoOceny)s.InfoOceny();
+                lp++;
+            }
         }
 
         public void InfoJednostki(bool infoWykladowcy)
         {
+            int lp = 1;
+            foreach (Jednostka j in jednostki)
+            {
+                Console.WriteLine("JEDNOSTKA NR. " + lp + ":");
+                j.WypiszInfo();
+                if (infoWykladowcy) j.InfoWykladowcy();
+                lp++;
+            }
 
         }
 
         public void InfoPrzedmioty()
         {
+            int lp = 1;
+            Console.WriteLine("Lista przedmiotów wykładanych na tym wydziale: ");
+            foreach(Przedmiot przedmiot in przedmioty)
+            {
+                Console.WriteLine(lp + ") " + przedmiot.Nazwa);
+                lp++;
+            }
 
         }
 
-        public bool DodajOcene(int nrIndeksu, string nazwaPrzedmiotu, int ocena, string data)
+        public bool DodajOcene(int nrIndeksu, string nazwaPrzedmiotu, double ocena, string data)
         {
-            return true;
+            bool result = false;
+            foreach(Przedmiot przedmiot in przedmioty)
+            {
+                if(przedmiot.Nazwa == nazwaPrzedmiotu)
+                {
+                    foreach(Student s in studenci)
+                    {
+                        if(s.NrIndeksu == nrIndeksu)
+                        {
+                            s.DodajOcene(nazwaPrzedmiotu, ocena, data);
+                            result = true;
+                        }
+                    }
+                    if (!result) Console.WriteLine("Mimo prawidłowej nazwy przedmiotu, nie udało się odnaleźć studenta o podanym numerz indeksu. Ocena nie zostanie dodana.");
+                }
+            }
+            if (!result) Console.WriteLine("Nie udało się odnaleźć przedmiotu o takiej nazwie. Ocena nie zostanie dodana..");
+            return result;
         }
 
         public bool UsunStudenta(int nrIndeksu)
         {
-            return false;
+            bool result = false;
+            foreach(Student student in studenci)
+            {
+                if(student.NrIndeksu == nrIndeksu)
+                {
+                    studenci.Remove(student);
+                    Console.WriteLine("Student o numerze indeksu " + nrIndeksu + " został prawidłowo usunięty z listy.");
+                    result = true;
+                }
+            }
+            if (!result) Console.WriteLine("Nie udało się odnaleźć na liście studenta o podanym numerze Indeksu..");
+            return result;
         }
 
         public bool PzeniesWykladowce(Wykladowca w, string obecnaJednostka, string nowaJednostka)
         {
-            return false;
+            bool result = false;
+            foreach(Jednostka j in jednostki)
+            {
+                if(j.Nazwa == obecnaJednostka && j.Wykladowcy.Contains(w))
+                {
+                    j.UsunWykladowce(w);
+                    foreach(Jednostka j2 in jednostki)
+                    {
+                        if(j2.Nazwa == nowaJednostka)
+                        {
+                            j2.DodajWykladowce(w);
+                            result = true;
+                        }
+                    }
+                    if (!result) Console.WriteLine("Coś poszło nie tak, nie udało się dodać Wykładowcy do nowej jednostki..");
+                }
+            }
+            if (!result) Console.WriteLine("Podany Wykładowca nie pracuje w jednostce podanej jako \"obecna\". Brak możliwości przeniesienia..");
+            return result;
         }
     }
 
     class Osoba : IInfo
     {
-        private string imie = "";
-        private string nazwisko = "";
-        private string dataUrodzenia = "";
+        protected string imie = "";
+        protected string nazwisko = "";
+        protected string dataUrodzenia = "";
 
         public Osoba(string imie, string nazwisko, string dataUrodzenia)
         {
@@ -86,6 +172,7 @@ namespace Prog_obiektowe_WSB_Projekt
         private int rok = 0;
         private int grupa = 0;
         private int nrIndeksu = 0;
+        private List<OcenaKoncowa> oceny = new List<OcenaKoncowa>();
 
         public Student(string imie, string nazwisko, string dataUrodzenia, string kierunek, string specjalnosc, int rok, int grupa, int nrIndeksu) : base(imie, nazwisko, dataUrodzenia)
         {
@@ -95,6 +182,16 @@ namespace Prog_obiektowe_WSB_Projekt
             this.grupa = grupa;
             this.nrIndeksu = nrIndeksu;
         }
+
+        public string ImieNazwisko
+        {
+            get
+            {
+                return imie + " " + nazwisko;
+            }
+        }
+
+        public int NrIndeksu { get { return nrIndeksu; }}
 
         public override void WypiszInfo()
         {
@@ -133,8 +230,12 @@ namespace Prog_obiektowe_WSB_Projekt
     {
         private string nazwa = "";
         private string adres = "";
+        private List<Wykladowca> wykladowcy = new List<Wykladowca>();
 
-        public Jednostka(string nazwa, string adres)
+            public string Nazwa { get { return nazwa; } }
+        public List<Wykladowca> Wykladowcy { get { return wykladowcy; } }
+
+            public Jednostka(string nazwa, string adres)
         {
             this.nazwa = nazwa;
             this.adres = adres;
@@ -182,6 +283,12 @@ namespace Prog_obiektowe_WSB_Projekt
             this.semestr = semestr;
             this.ileGodzin = ileGodzin;
         }
+
+        public string Nazwa 
+        { get
+            { return nazwa; } 
+        }
+
     }
 
     class OcenaKoncowa : IInfo
